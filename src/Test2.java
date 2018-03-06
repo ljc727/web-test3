c048622eabfe9a89a0e17d1ff0c193d269ac7f40
170119 <170119@AE0-W-D10055.ssg.global>
2018-03-06 14:21:17 +0900

    1

--- src/Test2.java
+++ src/Test2.java
@@ -7,12 +7,7 @@ public class Test2 {
 		System.out.println("222");
 		System.out.println("3333");
 		System.out.println("444");
-<<<<<<< HEAD
-		System.out.println("555");
-=======
-		System.out.println("555"));
-		System.out.println("6666");
->>>>>>> refs/heads/test3
+		
 	}
 
 }
